dc75459eafee492db757dfe896fa2c2c167783d9
cga815 <carolinealexander2018@u.northwestern.edu>
2016-05-02 17:35:55 -0500

    Map is functional and some clean up

--- samples/GeoGame.js
+++ samples/GeoGame.js
@@ -1,45 +1,36 @@
-
-/**
- * AmCharts.ready function handler is executed when the page loads
- * We are going to create both maps then. We'll store references to
- * all maps we create in the global maps variable so that we can
- * come back and get info from them later.
- */
+//Global Variables
 var maps = {};
+var map = new AmCharts.AmMap();
+var c = 0;
+var submitClicked=false;
+var countryToPick="US";
+var index;
 
+//Create and Render Map
 AmCharts.ready(function() {
   createMap('world');
-
 });
 
-AmCharts.makeChart( "chartdiv", {
+AmCharts.makeChart( "mapdiv", {
   "responsive": {
-    "enabled": true
-  }
-} );
+  "enabled": true,
+  },
+});
 
-/**
- * Creates a country map
- * $param country A map and container id
- */
 function createMap (country) {
-    var map = new AmCharts.AmMap();
-    var country = 0;
     maps[country] = map;
     map.pathToImages = "http://www.amcharts.com/lib/3/images/";
     map.panEventsEnabled = true;
     map.backgroundColor = "#666666";
     map.backgroundAlpha = 1;
-    
     map.zoomControl.panControlEnabled = false;
     map.zoomControl.zoomControlEnabled = true;
 
     var dataProvider = {
     map: country + "Low",
-        getAreasFromMap: true
+    getAreasFromMap: true,
     };
 
-    
     map.dataProvider = dataProvider;
     
     map.areasSettings = {
@@ -52,68 +43,52 @@ function createMap (country) {
         rollOverOutlineColor: "#FFFFFF",
         selectable: true,
         balloonText: "",
+        panEventsEnabled: true,
 
     };
-    
-    map.addListener('clickMapObject', function (event) {
-        // deselect the area by assigning all of the dataProvider as selected object
-        //map.showAsSelected= false
-        //map.selectedObject = map.dataProvider;
-        
-        //event.mapObject.showAsSelected = !event.mapObject.showAsSelected;
 
-        if(country==0)
+    //Add interactivity with map (can click on different countries)
+    map.addListener('clickMapObject', function (event) {
+        if(c!=0)
         {
-          var=mapObject;
+          c.showAsSelected=false;
+           map.returnInitialColor(c);
         }
-        // bring it to an appropriate color
+          c = event.mapObject;
+        
+        c.showAsSelected=true;
+
+     // An object is always selected after initial click
         map.returnInitialColor(event.mapObject);
     });
-
-//    map.addListener("clickMapObject", function (event) {
-//    alert( 'Clicked ID: ' + event.mapObject.id + ' (' + event.mapObject.title + ')' );
-//});
     
     map.write(country);
 }
 
-function handleFormSubmit() {
-  /**
-   * Gather selected areas from all maps and populate
-   * hidden fields with them
-   */
-   console.log(maps);
+//Update the map after submit is clicked
+ function submitChoice(){
+
   for(var x in maps) {
-  
     var map = maps[x];
-    var states = [];
     for (var i in map.dataProvider.areas) {
       var area = map.dataProvider.areas[i];
-      if (area.showAsSelected) {
-        states.push(area.id);
+      //disable selection for each country
+      area.selectable = false;
+      area.rollOverColor= "#CDCDCD";
+      area.rollOverOutlineColor= "#666666";
+      if (area.id==countryToPick) {
+        index=i;
       }
     }
-    document.getElementById(x + '_field').value = states.join(',');
   }
-  
-  /**
-   * Add submited info debug information (optional step)
-   * We're going to use jQuery to keep the code simple. However, jQuery
-   * is not necessary to run the maps - we're just using it here to
-   * print out debug info.
-   */
-  var formData = [];
-  jQuery('#user_form').find('input[type=hidden],input[type=text],input:checked').each(function () 
-  {
-    formData.push(this.name + ': <strong>' + this.value + '</strong>');
-    if(this.value=="US") formData.push("You found it!"); else formData.push("Thats not right..");
-  });
-  document.getElementById('form-debug').innerHTML = formData.join('<br />');
-  
-  console.log(formData)
-  /**
-   * Prevent form from actually submitting.
-   * We probably don't want it in real life situations.
-   */
-  return false;
-}
+    //color the wrong country picked
+    c.showAsSelected = true;
+    c.selectedColor="#FF0000";
+
+    //color the correct country picked, overwrites the above if they are the same
+    map.dataProvider.areas[index].showAsSelected = true;
+    map.dataProvider.areas[index].selectedColor="#00FF00";
+
+  map.validateData();
+
+};
